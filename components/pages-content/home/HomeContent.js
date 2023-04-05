a9d2c6f8a6f94e8c74f4d837a274e5fa6d253156
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-04-05 16:42:40 -0300

    .

--- components/pages-content/home/HomeContent.js
+++ components/pages-content/home/HomeContent.js
@@ -6,11 +6,8 @@ import Image from 'next/image';
 function HomeContent() {
     return <PageContainer id="home-page">
       <main className={`${globalStyles.contentSize} ${styles.contentPage}`}>
-        <div className={styles.homeImage}>
-          <Image alt="Logo Website" src={"/imgs/bright-light-logo-branca.webp"} height={50} width={50}/>
-        </div>
+        <div className={`${styles.homeImage} logo-website`}></div>
         <div>
-          
           <h1 className={styles.pageTitle}>Um projeto sobre cidades inteligentes!</h1>
         </div>
       </main>
