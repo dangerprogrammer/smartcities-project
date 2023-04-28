0a3b2de3acf9e8390b570e95309ba199b0142296
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-04-28 14:18:23 -0300

    .

--- components/pages-content/home/HomeContent.js
+++ components/pages-content/home/HomeContent.js
@@ -101,7 +101,7 @@ function animateItemsLoader() {
     props.x ??= randomInt(props.size, w - props.size);
     props.y ??= randomInt(props.size, h - props.size);
     props.color ??= '#25f';
-    props.speedY = (props.size ** -1) * -3e1;
+    props.speedY = props.size ** -1 * -3e1;
 
     const {size, x, y, color, speedY} = props;
 
