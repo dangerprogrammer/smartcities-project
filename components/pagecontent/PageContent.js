3e423123b47e26ccdae10124f92d041e58ce860c
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-04-28 19:25:33 -0300

    .

---
+++ components/pagecontent/PageContent.js
@@ -0,0 +1,8 @@
+import styles from './PageContent.module.scss';
+import globalStyles from '@/styles/globals.module.scss';
+
+function PageContent({children, ...argsContainer}) {
+
+};
+
+export default PageContent;
--- components/pages-content/home/HomeContent.js
+++ components/pages-content/home/HomeContent.js
@@ -1,11 +1,13 @@
 import PageContainer from '@/components/pagecontainer/PageContainer';
 import styles from './HomeContent.module.scss';
 import globalStyles from '@/styles/globals.module.scss';
-import Image from 'next/image';
 import {randomInt, random, between} from '@/scripts/global-scripts';
-import BackgroundContent from '@/components/background-content/backgroundContent';
+import BackgroundContent from '@/components/background-content/BackgroundContent';
+
+import Wave from '@/assets/svgs/wave-haikei.svg';
 
 function HomeContent() {
+  console.log(Wave);
   setTimeout(animateItemsLoader, 1);
 
   return <PageContainer id="home-page">
@@ -14,21 +16,26 @@ function HomeContent() {
       <div>
         <h1 className={styles.pageTitle}>Um projeto sobre cidades inteligentes!</h1>
       </div>
+      <header>Opa!</header>
       <BackgroundContent className={styles.homeBackground}/>
       <canvas className={styles.canvasElement} id="home-background"></canvas>
-      
+      <div>
+        <svg  id="wave" className={styles.waveBackground} viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1"><path d="M0 999L45.7 1002.2C91.3 1005.3 182.7 1011.7 274.2 1004.2C365.7 996.7 457.3 975.3 548.8 965.8C640.3 956.3 731.7 958.7 823 966.2C914.3 973.7 1005.7 986.3 1097 993C1188.3 999.7 1279.7 1000.3 1371.2 992.8C1462.7 985.3 1554.3 969.7 1645.8 970.5C1737.3 971.3 1828.7 988.7 1874.3 997.3L1920 1006L1920 1081L1874.3 1081C1828.7 1081 1737.3 1081 1645.8 1081C1554.3 1081 1462.7 1081 1371.2 1081C1279.7 1081 1188.3 1081 1097 1081C1005.7 1081 914.3 1081 823 1081C731.7 1081 640.3 1081 548.8 1081C457.3 1081 365.7 1081 274.2 1081C182.7 1081 91.3 1081 45.7 1081L0 1081Z" fill="#25f" strokeLinecap="round" strokeLinejoin="miter"></path></svg>
+      </div>
     </main>
   </PageContainer>
 };
 
 function animateItemsLoader() {
-  const canvas = document.querySelector('#home-background'), canvas2d = canvas.getContext('2d'), polygons = [];
+  const canvas = document.querySelector('#home-background'), wave = document.querySelector('#wave'), canvas2d = canvas.getContext('2d'), polygons = [];
+
+  console.log(wave.viewBox);
 
   let hasRender = !1;
 
   function updateCanvas() {
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+    wave.viewBox.baseVal.height = canvas.height = window.innerHeight;
+    wave.viewBox.baseVal.width = canvas.width = window.innerWidth;
 
     hasRender ? updatePolygons() : generatePolygons(20);
 
