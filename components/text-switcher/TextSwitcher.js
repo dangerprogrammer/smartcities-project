69ebebd929eadb6ad6d0ea896217300abf34a9b2
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-04-30 01:40:18 -0300

    .

--- components/text-switcher/TextSwitcher.js
+++ components/text-switcher/TextSwitcher.js
@@ -2,14 +2,16 @@ import styles from './TextSwitcher.module.scss';
 
 function TextSwitcher({switchedText, typeSpeed = 1e2, wordDuration = 2e3}) {
     let biggestWord;
-    setTimeout(() => biggestWord = setWords({switchedText, typeSpeed, wordDuration}), 1e2);
+    let loadSystem = setInterval(() => {
+        let word0 = document.querySelector('li[id*="shadow-"]');
+        if (!word0 || word0.offsetHeight + word0.offsetWidth === 0) return <span>Teste</span>;
+        else clearInterval(loadSystem);
 
-    let loadWordInt = setInterval(() => {
+        biggestWord = setWords();
+
         const parentWord = biggestWord.parentElement;
-        if (!biggestWord || !parentWord) return;
-        else clearInterval(loadWordInt);
 
-        loadSwitcher(parentWord, biggestWord);
+        loadSwitcher(parentWord, biggestWord, switchedText, typeSpeed, wordDuration);
     }, 1e2);
 
     return <>
@@ -21,10 +23,16 @@ function TextSwitcher({switchedText, typeSpeed = 1e2, wordDuration = 2e3}) {
     </>;
 };
 
-function loadSwitcher(parentWord, biggestWord) {
-    const aside = parentWord.children[0];
+function loadSwitcher(parentWord, biggestWord, switchedText, typeSpeed, wordDuration) {
+    const aside = parentWord.children[0], div = parentWord.children[1];
 
     aside.innerText = biggestWord.innerText;
+
+    switchedText.forEach((text, ind) => {
+        setTimeout(() => {
+            console.log("Opa!", (typeSpeed * text.length * 1.5 + wordDuration) * ind);
+        }, (typeSpeed + wordDuration) * ind);
+    });
 };
 
 function setWords() {
