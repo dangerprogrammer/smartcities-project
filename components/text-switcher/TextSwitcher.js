72fabe1537dafc8ad60317e498a3959fc9920a56
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-04-30 14:30:35 -0300

    .

--- components/text-switcher/TextSwitcher.js
+++ components/text-switcher/TextSwitcher.js
@@ -29,23 +29,28 @@ function loadSwitcher(parentWord, biggestWord, switchedText, typeSpeed, wordDura
 
     shadowRes.innerHTML = biggestWord.innerHTML;
 
-    let fullTimeout = 0, intervalCount = 0, firstLoad;
+    let fullTimeout = 0, intervalTime = 0, intervalCount = 0, startDelay = 0, firstLoad;
 
     if (startWord !== undefined) {
         firstLoad = !0;
         div.innerText = switchedText[startWord] || "";
+        
+        const findedWord = switchedText.find((text, ind) => ind === startWord);
 
-        switchedText.filter((text, ind) => ind <= startWord).forEach((text, ind) => {
-            fullTimeout -= typeSpeed * (switchedText[ind - 1] ? switchedText[ind - 1].length : 0) * 2.5 + wordDuration;
+        if (!findedWord) return;
 
-            const textStr = [...text.split('')];
-            textStr.forEach((str, indStr) => {
-                setTimeout(() => div.innerText = text.slice(0, text.length - (indStr + 1)), typeSpeed * text.length + wordDuration + (typeSpeed / 2) * indStr);
-            });
+        startDelay += typeSpeed * findedWord.length + wordDuration + (typeSpeed / 2) * findedWord.length;
+
+        const textStr = [...findedWord.split('')];
+        console.log(`writing word "${findedWord}"`);
+        textStr.forEach((str, indStr) => {
+            setTimeout(() => div.innerText = findedWord.slice(0, findedWord.length - (indStr + 1)), typeSpeed * findedWord.length + wordDuration + (typeSpeed / 2) * indStr);
         });
-    };
+    } else {
+        loadEachWord();
 
-    loadEachWord();
+        switchedText.forEach((text, ind) => intervalTime += typeSpeed * (switchedText[ind - 1] ? switchedText[ind - 1].length : 0) * 2.5 + wordDuration);
+    };
 
     let switchInterval = setInterval(() => {
         intervalCount++;
@@ -53,24 +58,27 @@ function loadSwitcher(parentWord, biggestWord, switchedText, typeSpeed, wordDura
         if (!infiniteSwitch) clearInterval(switchInterval);
 
         fullTimeout = 0;
+        firstLoad = !1;
 
         loadEachWord();
-    }, fullTimeout + wordDuration);
+    }, intervalTime + startDelay + wordDuration);
 
     function loadEachWord() {
+        console.log("loading words");
+
         switchedText.forEach((text, ind) => {
             fullTimeout += typeSpeed * (switchedText[ind - 1] ? switchedText[ind - 1].length : 0) * 2.5 + wordDuration;
 
-            if (startWord !== undefined && !(ind <= startWord && firstLoad)) setTimeout(() => {
-                const textStr = [...text.split('')];
-                textStr.forEach((str, indStr) => {
-                    setTimeout(() => div.innerText = text.slice(0, indStr + 1), typeSpeed * indStr);
-                    if (infiniteSwitch || ind !== switchedText.length - 1) setTimeout(() => div.innerText = text.slice(0, text.length - (indStr + 1)), typeSpeed * text.length + wordDuration + (typeSpeed / 2) * indStr);
-                });
-            }, fullTimeout);
+            if (startWord !== undefined && !(ind <= startWord && firstLoad)) {
+                setTimeout(() => {
+                    const textStr = [...text.split('')];
+                    textStr.forEach((str, indStr) => {
+                        setTimeout(() => div.innerText = text.slice(0, indStr + 1), typeSpeed * indStr);
+                        if (infiniteSwitch || ind !== switchedText.length - 1) setTimeout(() => div.innerText = text.slice(0, text.length - (indStr + 1)), typeSpeed * text.length + wordDuration + (typeSpeed / 2) * indStr);
+                    });
+                }, fullTimeout);
+            };
         });
-
-        firstLoad = !1;
     };
 };
 
