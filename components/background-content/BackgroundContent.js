471c106398dfa2f2de16eb6127f478191d72962c
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2023-06-12 16:44:21 -0300

    Delete components directory

--- components/background-content/BackgroundContent.js
+++
@@ -1,7 +0,0 @@
-import styles from './BackgroundContent.module.scss';
-
-function BackgroundContent({className}) {
-    return <aside className={`${styles.backgroundContent} ${className}`}></aside>
-};
-
-export default BackgroundContent;
--- components/footer/Footer.js
+++
@@ -1,7 +0,0 @@
-import styles from './Footer.module.scss';
-
-function Footer() {
-    return <footer className={styles.footer}>Footer</footer>
-};
-
-export default Footer;
